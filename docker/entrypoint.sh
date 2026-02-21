4c842207b083529d7042cab872bf69784cc731aa
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2026-02-21 17:41:32 +0530

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,16 +5,16 @@ PUID=${UID:-99}
 PGID=${GID:-100}
 
 # create a user and group with specified UID and GID
-addgroup -g $PGID appgroup
-adduser -D -u $PUID -G appgroup appuser
+addgroup -g "$PGID" appgroup 2>/dev/null || true
+adduser -D -u "$PUID" -G appgroup appuser 2>/dev/null || true
 
-mkdir -p $APP_HOME
-chown -R appuser:appuser $APP_HOME
+mkdir -p "$APP_HOME"
+chown -R appuser:appgroup "$APP_HOME"
 
 echo "Starting with UID: $PUID, GID: $PGID"
 
 # Fix permissions
-chown -R "$PUID":"$PGID" /config /input /output
+chown -R "$PUID":"$PGID" /config /input /output 2>/dev/null || true
 
 until cd /home/app/web
 do
@@ -31,13 +31,13 @@ done
 python manage.py collectstatic --noinput
 
 # Start Celery Worker
-gosu "$PUID":"$PGID" celery -A bragibooks_proj worker --loglevel=info --concurrency ${CELERY_WORKERS:-1} -E &
-
-# If you want to use the admin panel for debugging
-# python manage.py createsuperuser --noinput
+su-exec "$PUID:$PGID" celery -A bragibooks_proj worker \
+    --loglevel=info \
+    --concurrency "${CELERY_WORKERS:-1}" \
+    -E &
 
 # Start gunicorn server
-gosu "$PUID":"$PGID" gunicorn bragibooks_proj.wsgi \
+su-exec "$PUID:$PGID" gunicorn bragibooks_proj.wsgi \
     --bind 0.0.0.0:8000 \
     --timeout 1200 \
     --worker-tmp-dir /dev/shm \
@@ -45,6 +45,3 @@ gosu "$PUID":"$PGID" gunicorn bragibooks_proj.wsgi \
     --threads 4 \
     --worker-class gthread \
     --enable-stdio-inheritance
-
-# for debug
-#python manage.py runserver 0.0.0.0:8000
